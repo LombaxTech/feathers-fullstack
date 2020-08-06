73d1dcde59d09a49b40476319d8c3650604797d0
Rakib Khan <rakibkhan@live.co.uk>
2020-08-06 04:37:16 +0100

    .

--- server/src/services/users/users.class.js
+++ server/src/services/users/users.class.js
@@ -5,7 +5,10 @@ const validator = require("email-validator");
 exports.Users = class Users extends Service {
   async create(data, params) {
     if (!validator.validate(data.email)) {
-      throw new Error("Invalid email");
+      // throw new Error("Invalid email");
+      return {
+        error: "invalid email",
+      };
     }
     return super.create(data, params);
   }
