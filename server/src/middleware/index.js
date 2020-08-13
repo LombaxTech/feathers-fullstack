834d2dde0de22accd6cffc5d8c0c6d0ef8c75121
Rakib Khan <rakibkhan@live.co.uk>
2020-08-14 00:39:25 +0100

    learning to add custom middleware to feather app

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -1,5 +1,24 @@
+const createUserModel = require("../models/users.model");
+const createPostModel = require("../models/post.model");
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
+  app.get("/testuser", async (req, res) => {
+    // res.send(createUserModel(app));
+
+    const User = createUserModel(app);
+
+    let users = await User.find({});
+    res.json(users);
+  });
+
+  app.get("/testposts", async (req, res) => {
+    const Post = createPostModel(app);
+
+    let posts = await Post.find({}).limit(5);
+    // res.json(posts.length);
+    res.json(posts);
+  });
 };
